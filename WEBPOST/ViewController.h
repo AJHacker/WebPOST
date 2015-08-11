6ed5212ce601ab99dff47c5b6a82641390ab8120
AJHacker <arihant@quixoa.com>
2015-08-11 11:57:54 -0400

    init

--- WEBPOST/ViewController.h
+++ WEBPOST/ViewController.h
@@ -8,6 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    IBOutlet UITextView *outputText;
+     IBOutlet UITextField *field;
+     IBOutlet UIButton *b;
+    }
+-(IBAction)PostAction:(id) sender;
+-(IBAction)TextDoneEditing:(id) sender;
+
 
 @end
--- WEBPOST/ViewController.m
+++ WEBPOST/ViewController.m
@@ -13,7 +13,24 @@
 @end
 
 @implementation ViewController
-
+-(IBAction)TextDoneEditing:(id)sender{
+    [sender resignFirstResponder];
+}
+-(IBAction)PostAction:(id)sender{
+    NSString *post = [NSString stringWithFormat:@"text=%@\n", [field text]];
+    NSData *postdata = [post dataUsingEncoding: NSASCIIStringEncoding allowLossyConversion:NO];
+    NSString *postLength = [NSString stringWithFormat:@"%d", [postdata length]];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString:@"http://pixelatedthoughts.com/Pixelatedthoughts/cppbs/DMA/iospost.php"] ];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    [request setValue:@"MyApp-V1.0" forHTTPHeaderField:@"User-Agent"];
+    [request setHTTPBody:postdata];
+    NSData *urlData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSString *response = [[NSString alloc] initWithData:urlData encoding:NSASCIIStringEncoding];
+    [outputText setText:response];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
